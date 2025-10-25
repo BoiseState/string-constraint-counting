ed2750655c346f5ca94f434c12308ecd58d11ea2
natsteven <nathanaelsteven@u.boisestate.edu>
2025-10-25 16:59:07 -0600

    artifact init?

--- updateSPFJar.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# run mvn install to compile and build the latest version of MAS
-mvn install
-
-# copy the lates version of the MAS jar to the SPF lib directory
-cp target/string-constraint-solvers-*-dependencies.jar ../SPF/jpf-symbc/lib/MAS.jar
