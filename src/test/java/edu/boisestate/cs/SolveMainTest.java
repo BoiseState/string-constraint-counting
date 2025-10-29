e97d3bfca952dda2bc07248f6d92b088539f398b
natsteven <nathanaelsteven@u.boisestate.edu>
2025-10-29 16:21:51 -0600

    working on bug fixes

--- src/test/java/edu/boisestate/cs/SolveMainTest.java
+++ src/test/java/edu/boisestate/cs/SolveMainTest.java
@@ -34,30 +34,32 @@ public class SolveMainTest {
 //		SolveMain.main(args);
 //	}
 //
-//	// friendly reminder generator has bugs and is incomplete :)
-//	@Test
-//    public void smtQueryPlaygroundTest(){
-//        restoreStreams();
-//        try {
-//			// generator takes directory as input and converts smt2 files
-//			File file = new File("src/test/java/edu/boisestate/cs");
-//			String javaPath = "/usr/lib/jvm/java-21-openjdk-amd64/bin/java";
-//            ProcessBuilder pb = new ProcessBuilder(
-//					javaPath, "-cp",
-//					"/home/nat/Repos/SMT-parser-generator/target/GenJSONs-1.0-SNAPSHOT-jar-with-dependencies.jar",
-//					"edu.boisestate.cs.MainJSON",
-//					file.getAbsolutePath()
-//			);
-//			pb.redirectErrorStream(true);
-//            pb.start();
-//
-//			String[] args = {"output_cs/smt-input.smt2.json", "-s", "inverse", "-v", "2", "-l", "8"};
-//			SolveMain.main(args);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+	// friendly reminder generator has bugs and is incomplete :)
+	@Test
+    public void smtQueryPlaygroundTest(){
+        restoreStreams();
+		String jsonOutputPath = "src/test/resources/in/smt-input.smt2.json";
+        try {
+			// generator takes directory as input and converts smt2 files
+			File file = new File("src/test/resources/in/smt-input.smt2");
+			String javaPath = "/usr/lib/jvm/java-21-openjdk-amd64/bin/java";
+            ProcessBuilder pb = new ProcessBuilder(
+					javaPath, "-cp",
+					"/home/nat/Repos/SMT-parser-generator/target/GenJSONs-1.0-SNAPSHOT-jar-with-dependencies.jar",
+					"edu.boisestate.cs.MainJSON",
+					file.getAbsolutePath(),
+					jsonOutputPath
+			);
+			pb.redirectErrorStream(true);
+            pb.start();
+
+			String[] args = {jsonOutputPath, "-s", "inverse", "-v", "2", "-l", "8"};
+			SolveMain.main(args);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 	@Test
 	public void testLength() {
---
+++ src/test/resources/in/smt-input.smt2
@@ -0,0 +1,4 @@
+(declare-const s2_11 String)
+(declare-const s1_10 String)
+
+(assert (not (str.suffixof s2_11 s1_10)))
